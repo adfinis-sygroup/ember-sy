5529384aa32fe0c56e76351332e9c1d521f3de0f
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-08-18 11:27:18 +0200

    Loading dots component

---
+++ addon/components/sy-loading-dots.js
@@ -0,0 +1,8 @@
+import Ember  from 'ember'
+import layout from '../templates/components/sy-loading-dots'
+
+export default Ember.Component.extend({
+  layout,
+
+  classNames: [ 'loading-dots' ]
+})
