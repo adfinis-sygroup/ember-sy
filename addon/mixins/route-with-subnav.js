ac87d86a2b54dee29736ce59e839065c89c98601
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-12-09 17:49:25 +0100

    Added sidebar-toggler service and added it to route-with-subnav mixin

--- addon/mixins/route-with-subnav.js
+++ addon/mixins/route-with-subnav.js
@@ -1,4 +1,5 @@
-import Mixin from 'ember-metal/mixin'
+import Mixin         from 'ember-metal/mixin'
+import injectService from 'ember-service/inject'
 
 /**
  * Renders subnav route subroute into subnav outlet
@@ -7,6 +8,33 @@ import Mixin from 'ember-metal/mixin'
  * @public
  */
 export default Mixin.create({
+  /**
+   * Sidebar toggle service
+   *
+   * @property {SidebarTogglerService} sidebarToggler
+   * @public
+   */
+  sidebarToggler: injectService(),
+
+  /**
+   * Enable sidebar toggle button
+   *
+   * @method activate
+   * @return {void}
+   */
+  activate() {
+    this.get('sidebarToggler').enable()
+  },
+
+  /**
+   * Disable sidebar toggle button
+   *
+   * @method deactivate
+   * @return {void}
+   */
+  deactivate() {
+    this.get('sidebarToggler').disable()
+  },
 
   /**
    * Render subnav into subnav outlet
